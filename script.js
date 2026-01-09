0f3983cae60a336e469fb882dfcb7d1979ce9e62
Iri Takano <iri2121@icloud.com>
2026-01-09 18:35:46 -0500

    create empty array for library and Book constructor with title, author, pages, read, and addBooktoLibrary to add newBook to library array

---
+++ script.js
@@ -0,0 +1,13 @@
+const library = [];
+
+function Book(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = false;
+}
+
+function addBookToLibrary(title, author, pages, read){
+    let newBook = new Book(title, author, pages, read);
+    library.push(newBook);
+}
