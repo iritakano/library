05415f675b63dc2da94036ef4e07d8519a6f9804
Iri Takano <iri2121@icloud.com>
2026-01-09 19:40:46 -0500

    create displayLibrary function to display all books on screen

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
-const library = [];
+const myLibrary = [];
 
-function Book(title, author, pages, read){
+function Book(title, author, pages){
+    this.id = crypto.randomUUID();
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = false;
 }
 
-function addBookToLibrary(title, author, pages, read){
-    let newBook = new Book(title, author, pages, read);
-    library.push(newBook);
+function addBookToLibrary(title, author, pages){
+    let newBook = new Book(title, author, pages);
+    myLibrary.push(newBook);
+}
+
+const library = document.querySelector('.library');
+
+function displayLibrary() {
+    library.innerHTML = "";
+
+    myLibrary.forEach(book => {
+        const bookDiv = document.createElement("div");
+        bookDiv.classList.add('book');
+        const bookTitle = document.createElement('p');
+        const bookAuthor = document.createElement('p');
+        const bookPages = document.createElement('p');
+
+        bookTitle.textContent = `Title: ${book.title}`;
+        bookAuthor.textContent = `Author: ${book.author}`;
+        bookPages.textContent = `Pages: ${book.pages} pages`;
+
+        bookDiv.appendChild(bookTitle);
+        bookDiv.appendChild(bookAuthor);
+        bookDiv.appendChild(bookPages);
+
+        library.appendChild(bookDiv);
+    });
 }
